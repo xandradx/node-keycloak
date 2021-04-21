7f232fe01e10d2d6de8c74418a3250014bf22712
neosoft <neosoft@neosofts-MacBook-Pro.local>
2021-04-21 15:10:36 +0530

    Initial Boilerplate

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 import * as express from 'express';
 import { statusRoutes } from './components/status/StatusRoutes';
-import { authRoutes } from './components/auth/authRoutes';
-import { keyClockRoutes } from './components/keyclock/KeyclockRoutes';
 
 export function registerRoutes(app: express.Express): any {
 
@@ -9,15 +7,7 @@ export function registerRoutes(app: express.Express): any {
         response.render("home");
     });
 
-    const authRouter: any = express.Router();
-    app.use('/auth', authRouter);
-    authRoutes(app, authRouter);
-
     const statusRouter: any = express.Router();
     app.use('/api/status', statusRouter);
     statusRoutes(app, statusRouter);
-
-    const keyclockRouter: any = express.Router();
-    app.use('/api/keyclock', keyclockRouter);
-    keyClockRoutes(app, keyclockRouter);
 }
